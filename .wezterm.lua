1b4e1f48cf67e35b7a9e01552942eab3ec8ef6b5
Miguel Tulla <miguel12a.tulla@gmail.com>
2025-03-09 20:41:43 -0400

    initial commit

---
+++ .wezterm.lua
@@ -0,0 +1,20 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+config.font = wezterm.font("MesloLGS Nerd Font Mono")
+config.font_size = 19
+
+config.window_background_opacity = 1.0
+config.macos_window_background_blur = 10
+config.native_macos_fullscreen_mode = true
+
+-- tokyonight_night coolnight colorscheme:
+config.color_scheme = "tokyonight_night"
+
+-- and finally, return the configuration to wezterm
+return config
