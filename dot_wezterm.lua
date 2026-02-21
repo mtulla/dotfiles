d26db750322664c9e6270d087a9ad5be39d939b3
Miguel Tulla Lizardi <miguel.tullalizardi@datadoghq.com>
2026-02-21 02:50:07 -0500

    make wezterm fontsize smaller

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -7,7 +7,7 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 config.font = wezterm.font("MesloLGS Nerd Font Mono")
-config.font_size = 19
+config.font_size = 16
 
 config.window_background_opacity = 1.0
 config.macos_window_background_blur = 10
