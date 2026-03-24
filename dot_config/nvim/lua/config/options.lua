3927be19f193e199b5c0c999afd0c3ee9f5cdfd5
Miguel Tulla Lizardi <miguel.tullalizardi@datadoghq.com>
2026-03-24 18:32:18 -0400

    organize filetype specific tabs better

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -6,13 +6,3 @@
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = {"lua", "html"}, -- Apply to Lua files
-  desc = "Set specific tab length for Lua files",
-  callback = function()
-    -- Set buffer-local options for tab length (e.g., 2 spaces)
-    vim.opt.tabstop = 2
-    vim.opt.shiftwidth = 2
-    vim.opt.softtabstop = 2
-  end,
-})
