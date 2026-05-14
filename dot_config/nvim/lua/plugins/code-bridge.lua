6fb653f6087ebde445cbb7df4e83d8e015cd254f
Miguel Tulla <miguel.tullalizardi@datadoghq.com>
2026-05-14 22:52:37 +0000

    switch code-bridge to send context to codex

--- dot_config/nvim/lua/plugins/code-bridge.lua
+++ dot_config/nvim/lua/plugins/code-bridge.lua
@@ -2,6 +2,13 @@ return {
   "samir-roy/code-bridge.nvim",
   event = "VeryLazy",
   config = function()
-    require("code-bridge").setup()
+    require("code-bridge").setup({
+      tmux = {
+        target_mode = "window_name",
+        window_name = "codex",
+        process_name = "codex",
+        switch_to_target = true,
+      },
+    })
   end,
 }
